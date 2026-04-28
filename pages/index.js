8a7910ed072643ee760abc2099f12b5c17015d9b
Daniel Gusmão Soares <90197198+Dangsoares@users.noreply.github.com>
2026-04-28 02:29:19 +0000

    ajuste da frase para a pessoa especial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <h1>Oi,hoje é dia 27/04/2026 e estou iniciando o meu primeiro projeto web. Quando vc ver isso meu amor é porque consegui! Te Amo!!!</h1>
+}
+
+export default Home;
